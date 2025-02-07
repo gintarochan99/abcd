13cc1c8b27f507d2942b07e86deb5a5d819d1921
gintarochan99 <satoko.hamanaka.t6@dc.tohoku.ac.jp>
2025-02-07 10:40:52 +0900

    機能削減

--- app.py
+++ app.py
@@ -6,10 +6,5 @@ app = Flask(__name__)
 def home():
     return render_template('index.html')
 
-@app.route("/hello")
-def hello():
-    return"konnitiwa"
-
-
 if __name__ == '__main__':
     app.run(debug=True)
