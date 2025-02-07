3638372a24c646552400052dc1288bd2f10a56b0
gintarochan99 <satoko.hamanaka.t6@dc.tohoku.ac.jp>
2025-02-07 09:35:12 +0900

    2ks眼の利敵

--- app.py
+++ app.py
@@ -6,5 +6,10 @@ app = Flask(__name__)
 def home():
     return render_template('index.html')
 
+@app.route("/hello")
+def hello():
+    return"konnitiwa"
+
+
 if __name__ == '__main__':
     app.run(debug=True)
